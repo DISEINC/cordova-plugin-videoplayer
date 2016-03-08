67dac3ba758eb52e6a40e78567a28172a535a30f
Patrik Lindahl <patrik@dise.com>
2016-03-08 18:56:11 +0100

    Trying to figure out why we are leaking memory. This still crashes after a few hours on some devices.

--- src/android/VideoPlayer.java
+++ src/android/VideoPlayer.java
@@ -22,6 +22,8 @@ import android.widget.LinearLayout;
 import android.widget.VideoView;
 import android.view.View;
 
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
@@ -33,16 +35,30 @@ import org.json.JSONObject;
 public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, OnPreparedListener, OnErrorListener, OnDismissListener {
 
     protected static final String LOG_TAG = "VideoPlayer";
-
     protected static final String ASSETS = "/android_asset/";
-
     private CallbackContext callbackContext = null;
-
-    private Dialog dialog;
-
-    private VideoView videoView;
-
-    private MediaPlayer player;
+    private Dialog dialog_;
+    private VideoView videoView_;
+    private MediaPlayer player_;
+	private LinearLayout mainLayout_;
+	private String currentPath_;
+	
+    /**
+     * Initializes the plugin
+     *
+     * @param cordovaInterface    An interface to Cordova
+     * @param webView             The cordova web view
+     */
+	@Override
+	public void initialize(CordovaInterface cordovaInterface, CordovaWebView webView) {
+		super.initialize(cordovaInterface, webView);
+		
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				initVideoDialog();
+			}
+		});
+	}
 
     /**
      * Executes the request and returns PluginResult.
@@ -53,9 +69,9 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
      * @return              A PluginResult object with a status and message.
      */
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("play")) {
+        if (action.equals("play") || action.equals("prepare")) {
             this.callbackContext = callbackContext;
-
+			
             CordovaResourceApi resourceApi = webView.getResourceApi();
             String target = args.getString(0);
             final JSONObject options = args.getJSONObject(1);
@@ -75,7 +91,7 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
             // Create dialog in new thread
             cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
-                    openVideoDialog(path, options);
+                    openVideoDialog(path, options, action.equals("prepare"));
                 }
             });
 
@@ -86,23 +102,12 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
             callbackContext = null;
 
             return true;
-        }
-        else if (action.equals("close")) {
-            if (dialog != null) {
-                if(player.isPlaying()) {
-                    player.stop();
+        } else if (action.equals("close")) {
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    closeVideoDialog();
                 }
-                player.release();
-                dialog.dismiss();
-            }
-
-            if (callbackContext != null) {
-                PluginResult result = new PluginResult(PluginResult.Status.OK);
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-            }
-
+            });
             return true;
         }
         return false;
@@ -121,172 +126,160 @@ public class VideoPlayer extends CordovaPlugin implements OnCompletionListener, 
         }
         return uriString;
     }
+	
+    /**
+     * Initializes the video dialog and sets up all parameters in order
+     * to get things to work.
+     */
+	protected void initVideoDialog() {
+		LinearLayout mainLayout_ = new LinearLayout(cordova.getActivity());
+		mainLayout_.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		mainLayout_.setOrientation(LinearLayout.VERTICAL);
+		mainLayout_.setHorizontalGravity(Gravity.CENTER_HORIZONTAL);
+		mainLayout_.setVerticalGravity(Gravity.CENTER_VERTICAL);
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    protected void openVideoDialog(String path, JSONObject options) {
-        // Let's create the main dialog
-        dialog = new Dialog(cordova.getActivity(), android.R.style.Theme_NoTitleBar);
-        dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(true);
-        dialog.setOnDismissListener(this);
+		videoView_ = new VideoView(cordova.getActivity());
+		videoView_.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		videoView_.setOnPreparedListener(this);
+		videoView_.setOnCompletionListener(this);
+		videoView_.setOnErrorListener(this);
+		mainLayout_.addView(videoView_);
 		
-        // Hide system UI
-        int version = android.os.Build.VERSION.SDK_INT; // Device OS version
+		dialog_ = new Dialog(cordova.getActivity(), android.R.style.Theme_NoTitleBar);
+		dialog_.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog_.setCancelable(true);
+		dialog_.setOnDismissListener(this);
+		
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+		lp.copyFrom(dialog_.getWindow().getAttributes());
+		lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+		lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+		dialog_.setContentView(mainLayout_);
+		
+		// Hide system UI
+		int version = android.os.Build.VERSION.SDK_INT; // Device OS version
 		int uiOptions = 0;
-        if (version >= 14) { // (need API v.14)
-            uiOptions |= 0x00000002; // - View.SYSTEM_UI_FLAG_HIDE_NAVIGATION - hides nav bar
-        }
-        if (version >= 16) { // (need API v.16)
-            uiOptions |= 0x00000004 // - View.SYSTEM_UI_FLAG_FULLSCREEN - hides status bar
-                      |  0x00000100 // - View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                      |  0x00000200 // - View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                      |  0x00000400 // - View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            ;
-        }
-        /* From API v.19 the status bar and title bar can be rendered on top of a full-screen view.
-        * Below flags adds that if it's supported by the device OS */
-        if (version >= 19) { // (need API v.19)
-            uiOptions |= 0x00000800 // - View.SYSTEM_UI_FLAG_IMMERSIVE
-                      |  0x00001000 // - View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            ;
-        }
-        View decor = dialog.getWindow().getDecorView();
-        decor.setSystemUiVisibility(uiOptions);
+		if (version >= 14) { // (need API v.14)
+			uiOptions |= 0x00000002; // - View.SYSTEM_UI_FLAG_HIDE_NAVIGATION - hides nav bar
+		}
+		if (version >= 16) { // (need API v.16)
+			uiOptions |= 0x00000004 // - View.SYSTEM_UI_FLAG_FULLSCREEN - hides status bar
+					  |  0x00000100 // - View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					  |  0x00000200 // - View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					  |  0x00000400 // - View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			;
+		}
+		if (version >= 19) { // (need API v.19)
+			uiOptions |= 0x00000800 // - View.SYSTEM_UI_FLAG_IMMERSIVE
+					  |  0x00001000 // - View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			;
+		}
+		View decor = dialog_.getWindow().getDecorView();
+		decor.setSystemUiVisibility(uiOptions);
+		Log.i(LOG_TAG, "Init Done!");
+	}
 
-        // Main container layout
-        LinearLayout main = new LinearLayout(cordova.getActivity());
-        main.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-        main.setOrientation(LinearLayout.VERTICAL);
-        main.setHorizontalGravity(Gravity.CENTER_HORIZONTAL);
-        main.setVerticalGravity(Gravity.CENTER_VERTICAL);
+    /**
+     * Open the dialog and start playing the video
+     * @param path Path to the video file to play
+     * @param options Custom options
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    protected void openVideoDialog(String path, JSONObject options, boolean prepareOnly) {
+		try {
+			if (path.startsWith(ASSETS)) {
+				// TODO: This is not verified to work!
+				path = path.substring(15);
+			}
+			
+			if ((prepareOnly && !videoView_.isPlaying()) || !prepareOnly) {
+				if (currentPath_ != path) {
+					// Not the same video as before, set this new path
+					videoView_.setVideoPath(path);
+					currentPath_ = path;
+				} else {
+					// We want to play the same video as before, 
+					// just reset the position o the start
+					videoView_.seekTo(0);
+				}
+			}
 
-        videoView = new VideoView(cordova.getActivity());
-        videoView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-        // videoView.setVideoURI(uri);
-        // videoView.setVideoPath(path);
-        main.addView(videoView);
-
-        player = new MediaPlayer();
-        player.setOnPreparedListener(this);
-        player.setOnCompletionListener(this);
-        player.setOnErrorListener(this);
-
-        if (path.startsWith(ASSETS)) {
-            String f = path.substring(15);
-            AssetFileDescriptor fd = null;
-            try {
-                fd = cordova.getActivity().getAssets().openFd(f);
-                player.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
-            } catch (Exception e) {
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-                return;
-            }
-        }
-        else {
-            try {
-                player.setDataSource(path);
-            } catch (Exception e) {
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-                return;
-            }
-        }
-
-        try {
-            float volume = Float.valueOf(options.getString("volume"));
-            Log.d(LOG_TAG, "setVolume: " + volume);
-            player.setVolume(volume, volume);
-        } catch (Exception e) {
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-            result.setKeepCallback(false); // release status callback in JS side
-            callbackContext.sendPluginResult(result);
-            callbackContext = null;
-            return;
-        }
-
-        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            try {
-                int scalingMode = options.getInt("scalingMode");
-                switch (scalingMode) {
-                    case MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING:
-                        Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING");
-                        player.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-                        break;
-                    default:
-                        Log.d(LOG_TAG, "setVideoScalingMode VIDEO_SCALING_MODE_SCALE_TO_FIT");
-                        player.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-                }
-            } catch (Exception e) {
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                result.setKeepCallback(false); // release status callback in JS side
-                callbackContext.sendPluginResult(result);
-                callbackContext = null;
-                return;
-            }
-        }
-
-        final SurfaceHolder mHolder = videoView.getHolder();
-        mHolder.setKeepScreenOn(true);
-        mHolder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                player.setDisplay(holder);
-                try {
-                    player.prepare();
-                } catch (Exception e) {
-                    PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
-                    result.setKeepCallback(false); // release status callback in JS side
-                    callbackContext.sendPluginResult(result);
-                    callbackContext = null;
-                }
-            }
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                player.release();
-            }
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
-        });
-
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(dialog.getWindow().getAttributes());
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
-
-        dialog.setContentView(main);
-        dialog.show();
-        dialog.getWindow().setAttributes(lp);
+			if (!prepareOnly) {
+				// Start the video and show the window
+				dialog_.show();
+				videoView_.start();
+			}
+		} catch (Exception e) {
+			PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
+			result.setKeepCallback(false); // release status callback in JS side
+			callbackContext.sendPluginResult(result);
+			callbackContext = null;
+			return;
+		}
     }
+	
 
+    /**
+     * Close the dialog and stop playing the video
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	protected void closeVideoDialog() {
+		try {
+			if (dialog_ != null) {
+				if(videoView_.isPlaying()) {
+					videoView_.stopPlayback();
+				}
+				dialog_.dismiss();
+			}
+
+			if (callbackContext != null) {
+				PluginResult result = new PluginResult(PluginResult.Status.OK);
+				result.setKeepCallback(false); // release status callback in JS side
+				callbackContext.sendPluginResult(result);
+				callbackContext = null;
+			}
+		} catch (Exception e) {
+			PluginResult result = new PluginResult(PluginResult.Status.ERROR, e.getLocalizedMessage());
+			result.setKeepCallback(false); // release status callback in JS side
+			callbackContext.sendPluginResult(result);
+			callbackContext = null;
+			return;
+		}
+	}
+
+    /**
+     * Callback on error
+     * http://developer.android.com/reference/android/media/MediaPlayer.OnErrorListener.html
+     */
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
         Log.e(LOG_TAG, "MediaPlayer.onError(" + what + ", " + extra + ")");
-        if(mp.isPlaying()) {
-            mp.stop();
-        }
-        mp.release();
-        dialog.dismiss();
+        dialog_.dismiss();
         return false;
     }
 
+    /**
+     * Callback when the video is prepared
+     * http://developer.android.com/reference/android/media/MediaPlayer.OnPreparedListener.html
+     */
     @Override
     public void onPrepared(MediaPlayer mp) {
-        mp.start();
     }
 
+    /**
+     * Callback when the video is done
+     * http://developer.android.com/reference/android/media/MediaPlayer.OnCompletionListener.html
+     */
     @Override
     public void onCompletion(MediaPlayer mp) {
         Log.d(LOG_TAG, "MediaPlayer completed");
-        mp.release();
-        dialog.dismiss();
+        dialog_.dismiss();
     }
 
+    /**
+     * Callback when the dialog is dismissed
+     * http://developer.android.com/reference/android/content/DialogInterface.OnDismissListener.html
+     */
     @Override
     public void onDismiss(DialogInterface dialog) {
         Log.d(LOG_TAG, "Dialog dismissed");
